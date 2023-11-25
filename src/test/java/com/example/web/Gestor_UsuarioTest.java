20dd2779dde5835d75aaef94e1299a7cae854f46
JohanMantilla <johan.mantilla01@epn.edu.ec>
2023-11-24 19:12:45 -0500

    Refactorización clases JAVA

--- src/test/java/com/example/web/Gestor_UsuarioTest.java
+++ src/test/java/com/example/web/Gestor_UsuarioTest.java
@@ -6,15 +6,29 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Gestor_UsuarioTest {
-    Gestor_Usuario usuario = null;
+    Gestor_Usuario gestorUsuario = null;
+    Usuario usuario;
 
     @Before
-    public void setUp(){
-        usuario = new Gestor_Usuario();
+    public void setUp() {
+        gestorUsuario = new Gestor_Usuario();
+        usuario = crearUsuarioParaPrueba();
+    }
+
+    private static Usuario crearUsuarioParaPrueba() {
+        return new Usuario("Johan", "Roberto", "1752823649",
+                "as", "Estudiante", "0983706479", "Johans", "123");
     }
 
     @Test
     public void given_numberCC_when_validate_then_ok() {
-        assertTrue(usuario.validarCedulaEcuatoriana("1804669925"));
+        assertTrue(gestorUsuario.validarCedulaEcuatoriana("1804669925"));
     }
+
+    @Test
+    public void given_AnUser_when_addUser_then_timeout() {
+        assertEquals("Usuario agregado", gestorUsuario.agregarUsuario(usuario));
+    }
+
+
 }
